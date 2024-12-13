67e7520922e1288ed92d9308bd573d242e172879
popalexr <alexpop8@hotmail.com>
2024-12-13 09:03:33 +0200

    Updated font size in App.css and added buttons to sidebar in App.js

--- Front/src/App.js
+++ Front/src/App.js
@@ -16,6 +16,9 @@ const App = () => {
         <div className="sidebar">
             <button className="button background" id="home" onClick={handleClick}>Home</button>
             <button className="button background" id="angajati" onClick={handleClick}>Angajati</button>
+            <button className="button background" id="tasks" onClick={handleClick}>Taskuri</button>
+            <button className="button background" id="calendar" onClick={handleClick}>Calendar</button>
+            <button className="button background bottom" id="profile" onClick={handleClick}>Profil</button>
         </div>
     );
   };
